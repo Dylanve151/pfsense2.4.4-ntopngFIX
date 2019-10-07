d3ca82b0939c6d1b819f0a1a1221d11775e67144
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2019-10-07 22:19:54 +0200

    Update geoipupdate.sh

--- geoipupdate.sh
+++ geoipupdate.sh
@@ -41,16 +41,10 @@ _fetch() {
 }
 
 _fetch "https://${GEOIP_DB_SERVER}/download/geoip/database/GeoLite2-Country.tar.gz" GeoLite2-Country.mmdb
-#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" GeoIP.dat
-#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/GeoIPv6.dat.gz" GeoIPv6.dat
 
 if [ -n "$GEOIP_FETCH_CITY" ]; then
 	_fetch "https://${GEOIP_DB_SERVER}/download/geoip/database/GeoLite2-City.tar.gz" GeoLite2-City.mmdb
-	#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/GeoLiteCity.dat.gz" GeoLiteCity.dat
-	#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz" GeoLiteCityv6.dat
 fi
 if [ -n "$GEOIP_FETCH_ASN" ]; then
 	fetch "https://${GEOIP_DB_SERVER}/download/geoip/database/GeoLite2-ASN.tar.gz" GeoLite2-ASN.mmdb
-	#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/asnum/GeoIPASNum.dat.gz" GeoIPASNum.dat
-	#_fetch "http://${GEOIP_DB_SERVER}/download/geoip/database/asnum/GeoIPASNumv6.dat.gz" GeoIPASNumv6.dat
 fi
